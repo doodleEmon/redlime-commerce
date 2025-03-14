7e86fa736581f12923964de02cb9be8ffebedcc7
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-03-14 21:46:30 +0600

    category design done

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import dummyImg from "/reviewBg.png";
 
 const Category = () => {
   return (
-    <section className='mt-[80px] px-[130px] h-[953px] border'>
-      <h1 className='text-[48px] font-bold text-center'>Shop By{" "}<span className='text-transparent bg-clip-text bg-gradient-to-b from-yellow-800 to-yellow-200'>Category</span></h1>
+    <section className="mt-[80px] px-5 xl:px-[130px] border">
+      <h1 className="text-[48px] font-bold text-center">
+        Shop By{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-b from-yellow-800 to-yellow-200">
+          Category
+        </span>
+      </h1>
+      <div className="columns-1 sm:columns-2 lg:columns-3 py-10 md:py-20 gap-4">
+        <div className="mb-4 break-inside-avoid">
+          <img src={dummyImg} alt="" className="w-full object-cover rounded-lg" />
+        </div>
+        <div className="mb-4 break-inside-avoid">
+          <img src={dummyImg} alt="" className="w-full object-cover rounded-lg" />
+        </div>
+        <div className="mb-4 break-inside-avoid">
+          <img src={dummyImg} alt="" className="w-full object-cover rounded-lg" />
+        </div>
+        <div className="mb-4 break-inside-avoid">
+          <img src={dummyImg} alt="" className="w-full object-cover rounded-lg" />
+        </div>
+        <div className="mb-4 break-inside-avoid">
+          <img src={dummyImg} alt="" className="w-full object-cover rounded-lg" />
+        </div>
+        <div className="mb-4 break-inside-avoid">
+          <img src={dummyImg} alt="" className="w-full object-cover rounded-lg" />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
