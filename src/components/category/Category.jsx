e9ec161467c18f50403c5c27b885ee68c3b9a33b
Emon <150001421+doodleEmon@users.noreply.github.com>
2025-03-16 01:19:53 +0600

    category picture issue solved

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import dummyImg from "/reviewBg.png";
+import beautyBg from "../../assets/beauty.jpg";
+import fragrancesBg from "../../assets/fragrances.jpg";
+import furnitureBg from "../../assets/furniture.jpg";
+import groceriesBg from "../../assets/groceries.jpg";
 
 const Category = (props) => {
   const productList = props?.products?.data?.products || [];
@@ -7,23 +10,33 @@ const Category = (props) => {
   const uniqueCategories = [
     ...new Set(productList.map((product) => product.category)),
   ];
+
+  // Category-to-Image Mapping
+  const categoryImages = {
+    beauty: beautyBg,
+    fragrances: fragrancesBg,
+    furniture: furnitureBg,
+    groceries: groceriesBg,
+  };
+
   return (
     <section className="mt-[80px] px-5 xl:px-[130px]">
-      <h1 className="text-[48px] font-bold text-center">
+      <p className="text-[48px] font-anton text-center">
         Shop By{" "}
         <span className="text-transparent bg-clip-text bg-gradient-to-b from-yellow-800 to-yellow-200">
           Category
         </span>
-      </h1>
+      </p>
       <div className="grid grid-cols-3 gap-4 mt-12">
         {uniqueCategories?.map((category, index) => (
-          <div key={index} className="mb-4 break-inside-avoid relative">
+          <div key={index} className="mb-4 relative">
             <img
-              src={dummyImg}
+              src={categoryImages[category]}
               alt={category}
-              className="w-full object-cover rounded-lg"
+              className="w-[370px] h-[370px] object-cover rounded-lg"
+              loading="lazy"
             />
-            <p className="absolute left-5 bottom-5">{category}</p>
+            <p className="absolute left-5 top-5 text-[40px] capitalize bg-slate-300 bg-opacity-25">{category}</p>
           </div>
         ))}
       </div>
